7b98329e4c90e950503016cc2cb33449f628386f
DANIELJIMENEZH <159660355+DANIELJIMENEZH@users.noreply.github.com>
2024-03-05 16:10:32 -0600

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author FER
+ */
+public class calculadora {
+
+    public static void main(String[] args) {
+       int n1=Integer.parseInt(JOptionPane.showInputDialog( "Digita el número 1:" ));
+       int n2=Integer.parseInt(JOptionPane.showInputDialog( "Digita el número 2:" ));
+       
+       operaciones Op=new operaciones();
+       int suma=Op.sumar ( n1 , n2 );
+       int resta=Op.resta ( n1 , n2 );
+       int multiplicacion=Op.multiplicacion ( n1 , n2 );
+       int division=Op.divison ( n1 , n2 );
+       Op.mostrarResultados (suma,resta,multiplicacion,division);
+    }
+    }
+
+
